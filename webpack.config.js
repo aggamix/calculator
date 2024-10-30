946b3c05e18005f667feb75cd980c395e25db174
Mikita <nikitakuza1999@gmail.com>
2024-10-30 19:02:07 +0100

    Start project: add basic configs, webpack, '.gitignore' file and folders 'src' and 'dist'

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'Calculator',
+    template: './src/index.html'
+  })],
+};
